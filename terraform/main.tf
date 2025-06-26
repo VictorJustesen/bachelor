87b6df21c82a8ca075f648d89cf0db12b86d5757
Victor <gingerlord.and.co@gmail.com>
2025-06-26 09:30:05 +0200

    please work

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,7 +92,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  # Use the main principal ID of the AKS cluster for more reliable pull permissions
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
 
 resource "kubernetes_namespace" "app_ns" {
