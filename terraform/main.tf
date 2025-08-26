247fb1a4a36ab0a11179d189aaba8640e17458aa
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2025-08-26 21:33:01 +0200

    test fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "kubernetes_namespace" "app_ns" {
 
 # --- NEW: Create a Storage Account for the scraper data ---
 resource "azurerm_storage_account" "scraperdata" {
-  name                     = "bachelorscraperdata # A unique name is required
+  name                     = "bachelorscraperdata" # A unique name is required
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
